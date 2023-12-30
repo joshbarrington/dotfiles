ea3990766547a8085e2fa0362ca9991c1cf75ca5
joshbarrington <joshsbarrington@gmail.com>
2023-12-30 01:47:52 +0000

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,58 +1,58 @@
 local wezterm = require 'wezterm'
 local config = {}
+local action = wezterm.action
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 config.keys = {
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
-  },
-  { key = 'LeftArrow', mods = 'CMD|ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
-  { key = 'RightArrow', mods = 'CMD|ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
-  { key = 'DownArrow', mods = 'CMD|ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
-  { key = 'UpArrow', mods = 'CMD|ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
-  { key = 'Enter', mods = 'CMD|SHIFT', action = wezterm.action.TogglePaneZoomState },
+    -- Splitting and closing panes
+    { key = 'd', mods = 'CMD', action = action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'd', mods = 'CMD|SHIFT', action = action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'w', mods = 'CMD', action = action.CloseCurrentPane { confirm = false } },
+    -- Selecting active pane
+    { key = 'LeftArrow', mods = 'CMD|ALT', action = action.ActivatePaneDirection 'Left' },
+    { key = 'RightArrow', mods = 'CMD|ALT', action = action.ActivatePaneDirection 'Right' },
+    { key = 'DownArrow', mods = 'CMD|ALT', action = action.ActivatePaneDirection 'Down' },
+    { key = 'UpArrow', mods = 'CMD|ALT', action = action.ActivatePaneDirection 'Up' },
+    -- Maximise pane
+    { key = 'Enter', mods = 'CMD|SHIFT', action = action.TogglePaneZoomState },
+    -- Natural text editing
+    { mods = 'OPT', key = 'LeftArrow', action = action.SendKey { mods = 'ALT', key = 'b' } },
+    { mods = 'OPT', key = 'RightArrow', action = action.SendKey { mods = 'ALT', key = 'f' } },
+    { mods = 'CMD', key = 'LeftArrow', action = action.SendKey { mods = 'CTRL', key = 'a' } },
+    { mods = 'CMD', key = 'RightArrow', action = action.SendKey { mods = 'CTRL', key = 'e' } },
+    { mods = 'CMD', key = 'Backspace', action = action.SendKey { mods = 'CTRL', key = 'u' } },
 }
 
 config.color_scheme = 'GruvboxDarkHard'
 config.colors = {
-  foreground = 'bdbbb4',
-  background = '282727',
-  ansi = {
-    '282727', -- black
-    'cb231d', -- red
-    '98961a', -- green
-    'd69821', -- yellow
-    '458487', -- blue
-    '918b86', -- magenta
-    '689c69', -- cyan
-    'a89883', -- white
-  },
-  brights = {
-    '918b86', -- black
-    'fa4834', -- red
-    'b8ba26', -- green
-    'f9bc2f', -- yellow
-    '83a497', -- blue
-    'fb4833', -- magenta
-    '918b86', -- cyan
-    'bdbbb4', -- white
-  },
+    foreground = 'bdbbb4',
+    background = '282727',
+    ansi = {
+        '282727', -- black
+        'cb231d', -- red
+        '98961a', -- green
+        'd69821', -- yellow
+        '458487', -- blue
+        '918b86', -- magenta
+        '689c69', -- cyan
+        'a89883', -- white
+    },
+    brights = {
+        '918b86', -- black
+        'fa4834', -- red
+        'b8ba26', -- green
+        'f9bc2f', -- yellow
+        '83a497', -- blue
+        'fb4833', -- magenta
+        '918b86', -- cyan
+        'bdbbb4', -- white
+    },
 }
+
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font_size = 14
 
 return config
