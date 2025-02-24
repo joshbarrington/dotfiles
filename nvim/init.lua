b422b4e52f2b6d6517982fdc0194240e2f315fa4
Joshua Barrington <joshsbarrington@gmail.com>
2025-02-24 17:47:39 +0900

    latest nvim init

--- nvim/init.lua
+++ nvim/init.lua
@@ -267,7 +267,7 @@ require 'nvim-treesitter.install'.compilers = { "clang" }
 
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'markdown' },
+  ensure_installed = { 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'markdown', 'yaml', 'toml'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
