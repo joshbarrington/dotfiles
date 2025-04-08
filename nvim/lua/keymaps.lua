d397ff457e2ae5cfa6f7deefe67c611e8229b1b2
Joshua Barrington <joshsbarrington@gmail.com>
2025-04-08 17:37:29 +0200

    add hotkey for split resizing

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -28,3 +28,18 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- Split resize keymaps
+vim.keymap.set('n', '<M-j>', '<Cmd>resize +2<CR>', { noremap = true, silent = true, desc = 'Increase window height' })
+vim.keymap.set('n', '<M-k>', '<Cmd>resize -2<CR>', { noremap = true, silent = true, desc = 'Decrease window height' })
+vim.keymap.set(
+  'n',
+  '<M-l>',
+  '<Cmd>vertical resize +2<CR>',
+  { noremap = true, silent = true, desc = 'Increase window width' }
+)
+vim.keymap.set(
+  'n',
+  '<M-h>',
+  '<Cmd>vertical resize -2<CR>',
+  { noremap = true, silent = true, desc = 'Decrease window width' }
+)
