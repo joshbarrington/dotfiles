b58a0a575e24c8b69e808947d4aaaecf2a2072ba
Joshua Barrington <joshsbarrington@gmail.com>
2025-04-10 16:37:34 +0200

    gruv box diagnostic signs bg

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,6 +53,10 @@ require('lazy').setup({
       require('gruvbox').setup {
         overrides = {
           SignColumn = { bg = '#282828' },
+          DiagnosticSignError = { bg = '#282828', fg = '#fa4834' },
+          DiagnosticSignWarn = { bg = '#282828', fg = '#d69821' },
+          DiagnosticSignHint = { bg = '#282828', fg = '#689c69' },
+          DiagnosticSignInfo = { bg = '#282828', fg = '#458487' },
         },
       }
       vim.cmd.colorscheme 'gruvbox'
