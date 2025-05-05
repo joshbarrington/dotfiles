90184726dd467555c606e792584b621df46a8e98
Joshua Barrington <joshsbarrington@gmail.com>
2025-05-05 12:03:00 +0200

    add yaml file with brew installs, new start up bash script, remove playbook

--- README.md
+++ README.md
@@ -2,37 +2,13 @@
 
 ## Setup
 
-#### Install brew
-
-`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"`
-
 #### Generate GitHub SSH key
 
 - https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-
-#### Install Ansible / Run playbook
-
-- https://github.com/geerlingguy/mac-dev-playbook
-- replace contents of `default.config.yml` with `mac-dev-playbook/config.yml`
 
 #### Reset git remote to use SSH
 
 `git remote set-url origin git@github.com:USERNAME/REPOSITORY.git`
 
-#### Stow
-`stow -v -t $HOME home`
-
-#### Pipe brew.yaml list
-`yq e -r ".homebrew_installed_packages.[]" brew.yaml`
-
-#### Install oh-my-zsh
-
-`git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh`
-
-#### Set Starship icons
-`starship preset nerd-font-symbols > ~/.config/starship.toml`
-
-#### Misc Applications
-
-- [Spek](https://github.com/withmorten/spek-alternative/releases)
-- [FakinTheFunk](https://fakinthefunk.net/en/)
+#### Run startup
+`sh setup.sh`
---
+++ setup.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+set -e
+
+printf "${YELLOW}
+███████╗████████╗ █████╗ ██████╗ ████████╗    ██╗   ██╗██████╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝    ██║   ██║██╔══██╗
+███████╗   ██║   ███████║██████╔╝   ██║       ██║   ██║██████╔╝
+╚════██║   ██║   ██╔══██║██╔══██╗   ██║       ██║   ██║██╔═══╝
+███████║   ██║   ██║  ██║██║  ██║   ██║       ╚██████╔╝██║
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═════╝ ╚═╝
+${NC}"
+
+sudo printf "${YELLOW}Shell elevated with su permissions${NC}\n"
+
+require() {
+    command -v "${1}" &>/dev/null && return 0
+    printf "${RED}Missing required application: %s${NC}\n" "${1}" >&2
+    return 1
+}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # install linuxbrew dependencies
+    # install yq to parse yaml files
+    if require apt; then
+        sudo add-apt-repository ppa:rmescandon/yq
+        sudo apt update
+        sudo apt install -y build-essential procps curl file git yq
+    fi
+fi
+
+sudo printf "${YELLOW}Applying GNU Stow${NC}\n"
+rm -f ~/.zshrc
+mkdir -p ~/.config
+stow -v --adopt -t $HOME home
+
+if ! require brew; then
+    printf "${YELLOW}Installing Homebrew${NC}\n"
+    export NONINTERACTIVE=1
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [ -d "/home/linuxbrew/.linuxbrew/bin/brew"]; then
+            alias brew='sudo -Hu linuxbrew brew'
+        else
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        fi
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        if [ -d "/opt/homebrew/bin/brew"]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"
+        else
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            eval "$(/opt/homebrew/bin/brew shellenv)"
+        fi
+    fi
+    brew update
+else
+    printf "${YELLOW}Updating Homebrew${NC}\n"
+    brew update
+fi
+
+printf "${YELLOW}Installing Homebrew packages${NC}\n"
+brew install $(yq e -r ".homebrew_installed_packages.[]" packages.yaml)
+
+# Essentials to be installed on Ubuntu
+# - wezterm
+# - chrome
+# - fira-code-nerd-font
+# - sway/waybar
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# set starship config
+starship preset nerd-font-symbols > ~/.config/starship.toml
+
+sudo printf "${YELLOW}Setting zsh as default shell${NC}\n"
+command -v zsh | sudo tee -a /etc/shells
+sudo chsh -s $(which zsh) $(whoami)
+zsh -l
+
+printf "${GREEN}Setup complete${NC}\n"
