8dd381e6034a0b9e4f3dcaff72913d1070926229
Joshua Barrington <joshsbarrington@gmail.com>
2025-05-28 13:27:29 +0200

    change snacks scracth save location

--- home/.config/nvim/lua/plugins/snacks.lua
+++ home/.config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,7 @@ return {
   lazy = false,
   opts = {
     picker = { enabled = true },
-    scratch = { enabled = true },
+    scratch = { enabled = true, root = "~/k7-files/scratch" },
   },
   config = function()
     vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#928374" })
@@ -13,37 +13,37 @@ return {
   end,
   keys = {
     -- Top Pickers & Explorer
-    { "<leader><space>", function() require("snacks").picker.smart() end, desc = "Smart Find Files" },
-    { "<leader>/", function() require("snacks").picker.grep() end, desc = "Grep" },
+    { "<leader><space>", function() require("snacks").picker.smart() end,               desc = "Smart Find Files" },
+    { "<leader>/",       function() require("snacks").picker.grep() end,                desc = "Grep" },
     -- find
-    { "<leader>fb", function() require("snacks").picker.buffers() end, desc = "Buffers" },
-    { "<leader>ff", function() require("snacks").picker.files() end, desc = "Find Files" },
-    { "<leader>fg", function() require("snacks").picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fb",      function() require("snacks").picker.buffers() end,             desc = "Buffers" },
+    { "<leader>ff",      function() require("snacks").picker.files() end,               desc = "Find Files" },
+    { "<leader>fg",      function() require("snacks").picker.git_files() end,           desc = "Find Git Files" },
     -- git
-    { "<leader>gb", function() require("snacks").picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gl", function() require("snacks").picker.git_log() end, desc = "Git Log" },
-    { "<leader>gs", function() require("snacks").picker.git_status() end, desc = "Git Status" },
-    { "<leader>gS", function() require("snacks").picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>gd", function() require("snacks").picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    { "<leader>gb",      function() require("snacks").picker.git_branches() end,        desc = "Git Branches" },
+    { "<leader>gl",      function() require("snacks").picker.git_log() end,             desc = "Git Log" },
+    { "<leader>gs",      function() require("snacks").picker.git_status() end,          desc = "Git Status" },
+    { "<leader>gS",      function() require("snacks").picker.git_stash() end,           desc = "Git Stash" },
+    { "<leader>gd",      function() require("snacks").picker.git_diff() end,            desc = "Git Diff (Hunks)" },
     -- Grep
-    { "<leader>sb", function() require("snacks").picker.lines() end, desc = "Buffer Lines" },
-    { "<leader>sB", function() require("snacks").picker.grep_buffers() end, desc = "Grep Open Buffers" },
-    { "<leader>sw", function() require("snacks").picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>sb",      function() require("snacks").picker.lines() end,               desc = "Buffer Lines" },
+    { "<leader>sB",      function() require("snacks").picker.grep_buffers() end,        desc = "Grep Open Buffers" },
+    { "<leader>sw",      function() require("snacks").picker.grep_word() end,           desc = "Visual selection or word", mode = { "n", "x" } },
     -- search
-    { '<leader>s/', function() require("snacks").picker.search_history() end, desc = "Search History" },
-    { "<leader>sc", function() require("snacks").picker.command_history() end, desc = "Command History" },
-    { "<leader>sC", function() require("snacks").picker.commands() end, desc = "Commands" },
-    { "<leader>sd", function() require("snacks").picker.diagnostics() end, desc = "Diagnostics" },
-    { "<leader>sD", function() require("snacks").picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-    { "<leader>sh", function() require("snacks").picker.help() end, desc = "Help Pages" },
-    { "<leader>sH", function() require("snacks").picker.highlights() end, desc = "Highlights" },
+    { '<leader>s/',      function() require("snacks").picker.search_history() end,      desc = "Search History" },
+    { "<leader>sc",      function() require("snacks").picker.command_history() end,     desc = "Command History" },
+    { "<leader>sC",      function() require("snacks").picker.commands() end,            desc = "Commands" },
+    { "<leader>sd",      function() require("snacks").picker.diagnostics() end,         desc = "Diagnostics" },
+    { "<leader>sD",      function() require("snacks").picker.diagnostics_buffer() end,  desc = "Buffer Diagnostics" },
+    { "<leader>sh",      function() require("snacks").picker.help() end,                desc = "Help Pages" },
+    { "<leader>sH",      function() require("snacks").picker.highlights() end,          desc = "Highlights" },
     -- LSP
-    { "gd", function() require("snacks").picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gD", function() require("snacks").picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gr", function() require("snacks").picker.lsp_references() end, nowait = true, desc = "References" },
-    { "gI", function() require("snacks").picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gd",              function() require("snacks").picker.lsp_definitions() end,     desc = "Goto Definition" },
+    { "gD",              function() require("snacks").picker.lsp_declarations() end,    desc = "Goto Declaration" },
+    { "gr",              function() require("snacks").picker.lsp_references() end,      nowait = true,                     desc = "References" },
+    { "gI",              function() require("snacks").picker.lsp_implementations() end, desc = "Goto Implementation" },
     -- scratch
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>.",       function() Snacks.scratch() end,                               desc = "Toggle Scratch Buffer" },
+    { "<leader>S",       function() Snacks.scratch.select() end,                        desc = "Select Scratch Buffer" },
   },
 }
