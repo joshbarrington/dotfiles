b89494a88aa52330f6294abc35ac6f1861ccba36
Joshua Barrington <joshsbarrington@gmail.com>
2025-08-13 14:59:09 +0200

    later code companion conf

--- home/.config/nvim/lua/plugins/code-companion.lua
+++ home/.config/nvim/lua/plugins/code-companion.lua
@@ -3,22 +3,6 @@ return {
   dependencies = {},
   config = function()
     require('codecompanion').setup {
-      adapters = {
-        openrouter = function()
-          return require('codecompanion.adapters').extend('openai_compatible', {
-            env = {
-              url = 'https://openrouter.ai/api',
-              api_key = 'OPENROUTER_API_KEY',
-              chat_url = '/v1/chat/completions',
-            },
-            schema = {
-              model = {
-                default = 'deepseek/deepseek-chat-v3-0324:free',
-              },
-            },
-          })
-        end,
-      },
       strategies = {
         chat = {
           adapter = 'copilot',
